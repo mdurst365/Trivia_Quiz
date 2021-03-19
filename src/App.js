365326ce9c3babeb07cd759e6f99b5fb3c5be759
sziccardi1998 <sziccardifullstack@gmail.com>
2021-03-18 20:13:08 -0400

    All files moved over for authentication.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import HeaderInfo from "./components/HeaderInfo"
+// Import the useAuthTokenStore hook.
+import { useAuthTokenStore } from "./utils/auth";
 
 function App() {
+  // Use the hook to reauthenticate stored tokens.
+  useAuthTokenStore();
+
   return (
     <div className="App">
       <HeaderInfo/>
