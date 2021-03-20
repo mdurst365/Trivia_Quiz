3b6e9e72b8ba26a0052d3fccede0115c9ce04ed5
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-03-20 13:44:12 -0400

    updated server file with mongodb connection, updated api routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,18 +1,36 @@
 const db = require("../models");
 
 module.exports = function (app) {
-    app.get("/user", (req, res) => {
-        db.User.find({})
-          .then((userDB) => {
-            res.json(userDB);
-          })
-          .catch((err) => {
-            if (err) {
-              throw (err);
-            } else {
-              res.status(400);
-            }
-          });
+
+  app.get("/user", (req, res) => {
+    db.User.find({})
+      .then((userDB) => {
+        res.json(userDB);
+      })
+      .catch((err) => {
+        if (err) {
+          throw (err);
+        } else {
+          res.status(400);
+        }
       });
-      
+  });
+
+  app.post("/user", ({
+    body
+  }, res) => {
+    db.User.create(body)
+      .then((userDB) => {
+        res.json(userDB);
+      })
+      .catch((err) => {
+        if (err) {
+          throw (err);
+        } else {
+          res.status(400);
+        }
+      });
+  });
+
+
 };
