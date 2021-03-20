888ff7796e36b16ad03869e7171a2ec019d3614c
sziccardi1998 <sziccardifullstack@gmail.com>
2021-03-20 13:02:15 -0400

    redoing changes.

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+// API Routes will go here as they are needed
+
+
+
+// add before the catch all route
+app.use( "/api", require("./routes/authentication") );
+
+// If no routes are used send the react application=
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
+
+module.exports = router;
