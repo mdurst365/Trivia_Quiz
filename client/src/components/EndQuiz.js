78207ee59d07d1383e242ede3c082b4fdb68aab2
Molly Durst <mollymadegraphics@yahoo.com>
2021-03-22 16:48:01 -0400

    updated endquiz

--- client/src/components/EndQuiz.js
+++ client/src/components/EndQuiz.js
@@ -3,13 +3,19 @@ import "./assets/styles.css"
 import React from "react"
 import victory from "./assets/victory.jpg"
 import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom';
+
 
 function EndQuiz (){
     return (
         <div className="WelcomeBox space">
             <h1 className="smallHeaderBlue"><br />QUIZ NAME<br />&nbsp;</h1>
             <h1 className="space bold">Score:</h1>
+            
+            <Link to ="/scorescreen">
             <Button type="submit" variant="outlined">Save Name</Button>
+            </Link>
+            
             <div className="space"></div>
             <img className="imgRoundBorder" src={ victory } alt="Victory"></img>
             <h6>Photo by <a href="https://unsplash.com/@japhethmast?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Japheth Mast</a> on <a href="/s/photos/victory?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></h6>
