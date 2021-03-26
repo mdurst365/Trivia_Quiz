1ecf4970182cf157156e0386766b9c53c06c3692
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-03-26 17:52:16 -0400

    working on make quiz, building arrays for db storage

--- client/src/components/MakeQuiz.js
+++ client/src/components/MakeQuiz.js
@@ -7,48 +7,98 @@ import React, { useState, useEffect } from "react";
 
 
 function MakeQuiz() {
+
     const [count, setCount] = useState(1);
     const [formObj, setFormObj] = useState({});
+    const [questions, setQuestions] = useState([]);
+    const [answers, setAnswers] = useState([]);
+    const [correctAnswers, setcorrectAnswers] = useState([]);
+
+
+
+    // const [showElement, setShowElement] = useState(false);
+    
+
+    // increment question number
+    const handleCount = () => {
+        setCount(count + 1);
+    };
 
     // updates component state when the user types into the input field
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         setFormObj({ ...formObj, [name]: value })
-        console.log(setFormObj);
     };
 
-    const handleClick = () => {
-        setCount(count+1);
+
+    const handleNext = (event) => {
+        
+        event.preventDefault();
+
+        handleCount();
+
+        const {answerA, answerB, answerC, answerD, question, correctAnswer} = formObj;
+
+        setQuestions(questions.concat(question));
+        setAnswers(answers.concat(answerA, answerB, answerC, answerD));
+        setcorrectAnswers(correctAnswers.concat(correctAnswer));
+
+        console.log(questions);
+        console.log(answers);
+        console.log(correctAnswers);
     };
+
+    // const handleShowElement = () => {
+    //     setShowElement(!showElement);
+    // };
 
     return (
         <div className="WelcomeBox space">
-            <h1 class="smallHeader"><br />MAKE A QUIZ<br />&nbsp;</h1>
+            <h1 className="smallHeader"><br />MAKE A QUIZ<br />&nbsp;</h1>
             <h6 className="space smallright">Question: {count}/5</h6>
             <form>
                 <h3 className="formSmall">
                     <br />
-                    Category: <input type="text" placeholder="Category"></input>
-                    <div className="space"></div>
-                    Question: <input type="text" placeholder="Input Question"></input>
                     <div className="space">
-                        Answer A: <input type="text" placeholder="Answer A"></input>
+                        Quiz Title: <input name="title" onChange={handleInputChange} type="text" placeholder="Title"></input>
                     </div>
                     <div className="space">
-                        Answer B: <input type="text" placeholder="Answer B"></input>
+                        Category: <input name="category" onChange={handleInputChange} type="text" placeholder="Category"></input>
                     </div>
-                    <div className="space"></div>
-                    Answer C: <input type="text" placeholder="Answer C"></input>
-                    <div className="space"></div>
-                    Answer D: <input type="text" placeholder="Answer D"></input>
-                    <div className="space"></div>
-                    Correct: <input type="text" placeholder="Correct Answer"></input>
+                    <div className="space">
+                        Question: <input name="question" onChange={handleInputChange} type="text" placeholder="Input Question"></input>
+                    </div>
+                    <div className="space">
+                        Answer A: <input name="answerA" onChange={handleInputChange} type="text" placeholder="Answer A"></input>
+                    </div>
+                    <div className="space">
+                        Answer B: <input name="answerB" onChange={handleInputChange} type="text" placeholder="Answer B"></input>
+                    </div>
+                    <div className="space">
+                        Answer C: <input name="answerC" onChange={handleInputChange} type="text" placeholder="Answer C"></input>
+                    </div>
+                    <div className="space">
+                        Answer D: <input name="answerD" onChange={handleInputChange} type="text" placeholder="Answer D"></input>
+                    </div>
+                    <div className="space">
+                        Correct: <input name="correctAnswer" onChange={handleInputChange} type="text" placeholder="Correct Answer"></input>
+                    </div>
                 </h3>
             </form>
             <div className="space">
-                {/* <Link to="/makequizfinish"> */}
-                    <Button onClick={handleClick} type="submit" variant="outlined">Next</Button>
-                {/* </Link> */}
+
+                {count === 5 ? 
+                <div> <Button type="submit" variant="outlined">Back</Button> &nbsp;&nbsp;
+                <Link to="/selectquiz">
+                <Button type="submit" variant="outlined">Finish</Button> 
+                </Link>&nbsp;&nbsp;
+                </div>:
+                <div>
+                <Button onClick={handleClick} type="submit" variant="outlined">Next</Button> &nbsp;&nbsp;
+                <Button type="submit" variant="outlined">Back</Button> &nbsp;&nbsp;
+                </div>
+                }   
+
             </div>
         </div>
     );
