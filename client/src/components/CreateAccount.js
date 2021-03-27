0c32468dad67874dc3e27e935eb9d451db6408ea
mohamedahmed-1980 <mohamedahmed0912@hotmail.com>
2021-03-27 12:08:02 -0400

    create account console loge

--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -1,40 +1,42 @@
-import "./assets/css_reset.css"
+import React, { useState } from "react";
+import "./assets/css_reset.css";
 import "./assets/styles.css";
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
 function CreateAccount() {
+    const [enteredUserName, setEnteredUserName] = useState("");
+    const [enteredPassword, setEnteredPassword] = useState("");
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log("username is " + enteredUserName);
+        console.log("password is " + enteredPassword);
+    };
     return (
         <div className="WelcomeBox space">
-            <h1 class="smallHeader"><br />WELCOME<br />&nbsp;</h1>
-            <p class="space bold">REGISTERED USERS</p>
-            <form>
+            <h1 className="smallHeader"><br />WELCOME<br />&nbsp;</h1>
+            <p className="space bold">Create Your Account</p>
+            <form onSubmit={handleSubmit}>
                 <br />
-                    Username: <input type="username" placeholder="Username"></input>
+                    Username: <input type="username" placeholder="Username"
+                    onChange={event => {
+                        setEnteredUserName(event.target.value)
+                    }}></input>
                 <br /><br />
-                    Password: <input type="password" placeholder="Password"></input>
+                    Password: <input type="password" placeholder="Password"
+                    onChange={event => {
+                        setEnteredPassword(event.target.value)
+                    }}
+                >
+                </input>
+                <p className="space">
+                    <Button type="submit" variant="outlined">
+                        submit
+                </Button>
+                </p>
             </form>
-            <p className="space">
-                <Button type="submit" variant="outlined">
-                    <Link to="/MakeTake">
-                        Sign In
-                    </Link>
-                </Button>
-            </p>
-            <p className="space">
-                <Button type="submit" variant="outlined">
-                    <Link to="/CreateAccount">
-                        Creat Account
-                    </Link>
-                </Button>
-            </p>
-            <p className="space bold">LOGIN AS GUEST
-                <br /><br />
-                <Button type="submit" variant="outlined">
-                    <Link to="/MakeTake">
-                        Login
-                    </Link>
-                </Button></p>
+
+
         </div>
     );
 }
