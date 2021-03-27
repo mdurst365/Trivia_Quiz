e6480b4fe2c24371204cb45c46b137082258e61a
mohamedahmed-1980 <mohamedahmed0912@hotmail.com>
2021-03-27 08:36:36 -0400

    removing logout bottun

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -7,15 +7,9 @@ function Registration() {
     return (
         <div className="WelcomeBox space">
             <h1 class="smallHeader"><br />WELCOME<br />&nbsp;</h1>
-<<<<<<< HEAD
             <p class="space bold">REGISTERED USERS</p>
             <form>
                 <br />
-=======
-                <p class="space bold">LOG-IN OR SIGN UP</p>
-                <form>
-                    <br/>
->>>>>>> c2e81eab28a3da07307772ea06e59d663e9fe7ab
                     Username: <input type="username" placeholder="Username"></input>
                 <br /><br />
                     Password: <input type="password" placeholder="Password"></input>
@@ -23,25 +17,7 @@ function Registration() {
             <p className="space">
                 <Button type="submit" variant="outlined">
                     <Link to="/MakeTake">
-<<<<<<< HEAD
                         Sign In
-=======
-                    Register
-                    </Link>
-                    </Button>
-                    </p>
-                    <p className="space">
-                    <Button type="submit" variant="outlined">
-                    <Link to="/MakeTake">
-                    Sign In
->>>>>>> c2e81eab28a3da07307772ea06e59d663e9fe7ab
-                    </Link>
-                </Button>
-            </p>
-            <p className="space">
-                <Button type="submit" variant="outlined">
-                    <Link to="/MakeTake">
-                        Logout
                     </Link>
                 </Button>
             </p>
