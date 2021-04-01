d8b7666ddefbf476f0b2f6faa188d759bd0057cb
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-04-01 13:54:47 -0400

    testing heroku deploy with dev branch

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use([
 
 // Serve static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
 // Link API Routes here
