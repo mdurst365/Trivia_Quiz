81753496234bef1110c6b160a43ced087669249a
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-04-02 11:14:35 -0400

    merged server and index js files

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
-require("dotenv").config();
-
-const passport = require("passport");
-
-app.use(passport.initialize());
-// Passport config
-passport.use( require("./config/jwtPassportStrategy") );
-
+const express = require("express");
+const logger = require("morgan"); 
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const app = express();
+require("dotenv").config();
+const passport = require("passport");
+
+// Port
 const PORT = process.env.PORT || 3001;
 
-// define middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Setup Express server
+const app = express();
+
+app.use([
+  express.urlencoded({ extended: true }),
+  express.json(),
+  passport.initialize(),
+  // Enable morgan log
+  logger("dev")
+]);
+
+// Serve static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/public"));
+}
+
+// Passport config
+passport.use(require("./config/jwtPassportStrategy"));
+
+// Link API Routes here
+app.use(routes);
 
 // authentication
 app.use( "/api", require("./routes/authentication") );
 
+// Mongo DB connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/trivia_db");
 
-app.use(passport.initialize());
-// Passport config
-passport.use( require("./config/jwtPassportStrategy") );
+app.listen(PORT, () => {
+  console.log("🚀  Server now on port", PORT, "👻 React App on Port 3000");
+});
